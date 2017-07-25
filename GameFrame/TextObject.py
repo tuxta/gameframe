3062d77beaa6ee5405be6e4320ff8e8a590d5cff
Steven Tucker <tuxta2@gmail.com>
2017-07-25 21:36:54 +1000

    Added class to handle text objects in the room. Inherits from RoomObject, so can be used just as any other object (collisions etc), but has the extra ability to define the font type, size and if it is bolded.

---
+++ GameFrame/TextObject.py
@@ -0,0 +1,24 @@
+import pygame
+from GameFrame import RoomObject
+
+
+class TextObject(RoomObject):
+    def __init__(self, room, x, y, text='Not Set', size=60, font='Comic Sans MS', colour=(0, 0, 0), bold=False):
+        RoomObject.__init__(self, room, x, y)
+
+        self.rendered_text = 0
+        self.rect = 0
+        self.built_font = 0
+        self.text = text
+        self.size = size
+        self.font = font
+        self.colour = colour
+        self.bold = bold
+        self.update_text()
+
+    def update_text(self):
+        self.built_font = pygame.font.SysFont(self.font, self.size, self.bold)
+        self.rendered_text = self.built_font.render(self.text, False, self.colour)
+        self.image = self.rendered_text
+        self.width, self.height = self.built_font.size(self.text)
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
--- GameFrame/__init__.py
+++ GameFrame/__init__.py
@@ -1,4 +1,4 @@
 from GameFrame.Level import Level
 from GameFrame.Globals import Globals
 from GameFrame.RoomObject import RoomObject
-
+from GameFrame.TextObject import TextObject

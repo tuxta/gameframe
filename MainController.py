f333ea73cdcd0efc1f02d56438d721dde6820dbd
tuxta <tuxta2@gmail.com>
2017-09-24 12:30:30 +1000

    Set Level progression as a comment

--- MainController.py
+++ MainController.py
@@ -14,28 +14,30 @@ window_size = (Globals.SCREEN_WIDTH, Globals.SCREEN_HEIGHT)
 screen = pygame.display.set_mode(window_size,
                                  pygame.DOUBLEBUF, 32)
 
-# - Track current level - #
-start_screen, level_1, level_2, level_3, end_screen, game_over = 0, 1, 2, 3, 4, 5
-curr_level = start_screen
 
 # - Set the starting number of lives - #
 Globals.LIVES = 3
 
 
+
+
+"""
+  This is an Example loop that controls progression through levels.
+  Allows multiple uses of a level if desired
+  (may provide a parameter to a level to indicate a
+   repeat visit for instance)
+
+# - Track current level - #
+start_screen, level_1, level_2, level_3, end_screen, game_over = 0, 1, 2, 3, 4, 5
+curr_level = start_screen
+
 def run_level(room):
-    print('here')
     exit_val = room.run()
     if exit_val is True or Globals.running is False:
         global curr_level
         global end_screen
         curr_level = end_screen
 
-"""
-  This loop controls progression through the levels.
-  Allows multiple uses of a level if desired
-  (may provide a parameter to a level to indicate a
-   repeat visit for instance)
-"""
 while Globals.running:
 
     if curr_level == start_screen:
@@ -59,5 +61,6 @@ while Globals.running:
 
     else:
         Globals.running = False
+"""
 
 sys.exit()

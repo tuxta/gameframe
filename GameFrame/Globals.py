3dcdd67dc4626a4e92be2c84475f00a991e80d0f
tuxta <tuxta2@gmail.com>
2017-12-11 13:50:23 +1000

    Moved Game variables out of the MainController and into the Global File. This way the MainController does not need to be edited at all.

--- GameFrame/Globals.py
+++ GameFrame/Globals.py
@@ -7,5 +7,27 @@ class Globals:
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 600
 
-    LIVES = 0
     SCORE = 0
+
+    # - Set the starting number of lives - #
+    LIVES = 3
+
+    # - Set the Window display name - #
+    window_name = 'GF Game'
+
+    # - Set the order of the rooms - #
+    levels = ["WelcomeScreen", "Maze", "ScrollingShooter", "BreakOut"]
+
+    # - Set the starting level - #
+    start_level = 0
+
+    # - Set this number to the level you want to jump to when the game ends - #
+    end_game_level = 4
+
+
+# ############################################################# #
+# ###### User Defined Global Variables below this line ######## #
+# ############################################################# #
+
+    total_count = 0
+    destroyed_count = 0
--- MainController.py
+++ MainController.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-
 import pygame
-
 from GameFrame import Globals
 
 pygame.mixer.pre_init(44100, -16, 2, 2048)
@@ -11,21 +9,13 @@ pygame.mixer.init()
 pygame.init()
 pygame.font.init()
 
-# - Set the Window Title for the game - #
-pygame.display.set_caption("GF Game")
+pygame.display.set_caption(Globals.window_name)
 window_size = (Globals.SCREEN_WIDTH, Globals.SCREEN_HEIGHT)
 screen = pygame.display.set_mode(window_size,
                                  pygame.DOUBLEBUF, 32)
 
-
-# - Set the starting number of lives - #
-Globals.LIVES = 3
-
-# - Set the order of the rooms - #
-levels = ["Level_1", "Level_2", "Level_3"]
-# - Set the starting level - #
-next_level = 0
-
+next_level = Globals.start_level
+levels = Globals.levels
 
 # - Main Game Loop. Steps through the levels defined in levels[] - #
 while Globals.running:
@@ -41,8 +31,7 @@ while Globals.running:
 
     if exit_val is True or Globals.running is False:
 
-        # - Set this number to the level you want to jump to when the game ends - #
-        next_level = 0
+        next_level = Globals.end_game_level
 
         if len(levels) == 1:
             break

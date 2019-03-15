a5fbe2ba5f1536d97f2d045b523d23830cf9d5b8
tuxta <tuxta2@gmail.com>
2019-03-15 18:16:37 +1000

    Added Database class for saving and retrieving game data

---
+++ GameFrame/DataBaseController.py
@@ -0,0 +1,30 @@
+import os
+import sqlite3
+
+
+class DataBaseController:
+    def __init__(self):
+        dbase_file_name = 'GameDatabase'
+        app_file_path = os.path.join(os.path.dirname(__file__), dbase_file_name)
+        self.app_db = sqlite3.connect(app_file_path)
+        self.app_cursor = self.app_db.cursor()
+
+    def close_databases(self):
+        self.app_db.close()
+
+    # ------------------------------ #
+    # -- This is an example query -- #
+    # ------------------------------ #
+'''
+    def get_lesson(self, topic):
+        self.app_cursor.execute(
+            """
+                SELECT lesson
+                FROM Topic
+                WHERE name = :topic
+            """,
+            {'topic': topic}
+        )
+        return self.app_cursor.fetchone()[0]
+'''
+
